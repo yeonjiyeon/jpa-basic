c3839757b5968d4b9c1d5462f02fe0f59e8e8e9f
yeonjiyeon <dalnim95@naver.com>
2021-11-14 13:32:10 +0900

    okk

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -16,18 +16,29 @@ public class JpaMain {
         tx.begin();
 
         try {
-            //영속
+            //저장
+            Team team = new Team();
+            team.setName("TeamA");
+            em.persist(team);
+
             Member member = new Member();
-            member.setId(2L);
-            member.setUsername("B");
-            member.setRoleType(RoleType.ADMIN);
+            member.setUsername("member1");
+            member.setTeam(team);
             em.persist(member);
+
+            em.flush();
+            em.clear();
+
+            Member findMember = em.find(Member.class, member.getId());
+
+            Team findTeam = findMember.getTeam();
+            System.out.println("findTeam.getName() = " + findTeam.getName());
 
 
             tx.commit();
-        }catch (Exception e){
+        } catch (Exception e) {
             tx.rollback();
-        }finally {
+        } finally {
             em.close();
         }
         emf.close();
--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -5,31 +5,31 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.Date;
 
-@Entity(name = "Member")//디폴트값은 클래스명과 동일하다 보통 축약어를 사용하기도 한다 예 )MBR
-//@Table(name = "USER") 쿼리값을 넣어줄 테이블명을 변경할 수 있다 디폴트는 클래스명
+@Entity
 public class Member {
     @Id
+    @GeneratedValue
+
+    @Column(name = "MEMBER_ID")
     private Long id;
-    @Column(name = "name")
+
+    @Column(name = "USERNAME")
     private String username;
-    private Integer age;
-    @Enumerated(EnumType.STRING)//db에는 enum타입이 없기 때문에 비슷한 Enumerated를 사용한다
-    private RoleType roleType;
-    @Temporal(TemporalType.TIMESTAMP)
-    private Date createdDate;
-    @Temporal(TemporalType.TIMESTAMP)
-    private Date lastModifiedDate;
 
-    /*hibernate 최신버전에서는 애노테이션없어도 자동으로 데이터타입을 맞춰서 생성해준다
-    private LocalDate testLocalDate;
-    private LocalDateTime testLocalDateTime;
-*/
-    
-    @Lob//내용일 긴 것을 넣고 싶을 때 사용
-    private String description;
+//    @Column(name = "TEAM_ID")
+//    private Long teamId;
 
-    public Member(){}
-//jpa는 기본적으로 내부적으로 리플렉션같은 것을 쓰기 때문에 동적으로 객체를 생성해내야 한다-> 기본생성자가 필요하다
+    @ManyToOne//멤버입장에서(앞)
+    @JoinColumn(name = "TEAM_ID")//외래키와 매핑
+    private Team team;//직접 가져오는 것으로 바꿈
+
+    public Team getTeam() {
+        return team;
+    }
+
+    public void setTeam(Team team) {
+        this.team = team;
+    }
 
     public Long getId() {
         return id;
@@ -46,44 +46,5 @@ public class Member {
     public void setUsername(String username) {
         this.username = username;
     }
-
-    public Integer getAge() {
-        return age;
-    }
-
-    public void setAge(Integer age) {
-        this.age = age;
-    }
-
-    public RoleType getRoleType() {
-        return roleType;
-    }
-
-    public void setRoleType(RoleType roleType) {
-        this.roleType = roleType;
-    }
-
-    public Date getCreatedDate() {
-        return createdDate;
-    }
-
-    public void setCreatedDate(Date createdDate) {
-        this.createdDate = createdDate;
-    }
-
-    public Date getLastModifiedDate() {
-        return lastModifiedDate;
-    }
-
-    public void setLastModifiedDate(Date lastModifiedDate) {
-        this.lastModifiedDate = lastModifiedDate;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
+    
 }
